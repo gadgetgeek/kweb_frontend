f98957bc638037f2ae77cd0962584c96abdfe4a4
Kerlin <lopeskerlin@gmail.com>
2021-11-22 20:44:21 -0500

    changes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
     <React.StrictMode>
       <App />
       <h2>Welcome to the Grocery List App!</h2>
-      <h3>Add any items you need from the store so you can take your grocery list anywhere with our Grocery List app</h3>
+      {/* <h3>Add any items you need from the store so you can take your grocery list anywhere with our Grocery List app</h3> */}
       <img src="https://i.imgur.com/iMP6Mvz.jpeg" className="app-logo" alt=""/>
     </React.StrictMode>
   </Router>,
